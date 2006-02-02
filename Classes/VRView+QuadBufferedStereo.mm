469c5d2e4da15c5f1ab24735275343d75e93ba05
me <me@JonathonMah.com>
2006-02-01 16:30:05 -0800

    Reversed VRView buffer reading due to inverted stereo bug
    darcs-hash:20060202003005-f9851-9ec4a97d2cbe8efbf8575890623706cb6332bae1.gz

--- Classes/VRView+QuadBufferedStereo.mm
+++ Classes/VRView+QuadBufferedStereo.mm
@@ -140,10 +140,13 @@ static NSMutableDictionary *ivarMapping = nil; // Keys are NSValue objects with 
 	if (renderWindow->GetStereoRender() && (renderWindow->GetStereoType() == VTK_STEREO_CRYSTAL_EYES) && bufferString)
 	{
 		renderWindow->MakeCurrent();
+#warning These are reversed due to the inverted stereo bug
 		if ([bufferString isEqualToString:@"Left"])
-			glReadBuffer(GL_FRONT_LEFT);
-		else if ([bufferString isEqualToString:@"Right"])
+			//glReadBuffer(GL_FRONT_LEFT);
 			glReadBuffer(GL_FRONT_RIGHT);
+		else if ([bufferString isEqualToString:@"Right"])
+			//glReadBuffer(GL_FRONT_RIGHT);
+			glReadBuffer(GL_FRONT_LEFT);
 	}
 	
 	return [self QBS_nsimage:YES];
